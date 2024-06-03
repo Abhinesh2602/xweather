1267277e29a57639d3a7a581c15a6de9f8e66c27
Abhinesh <abhineshgoud72@gmail.com>
2024-06-03 13:36:37 +0530

    changed css format

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
+import { useState } from "react";
+import "./App.css";
 
-import search from "./assets/search.svg";
 import fetchData from "./fetchLocation";
 
 function App() {
@@ -31,18 +30,18 @@ function App() {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <form className={styles.formContainer} onSubmit={handleSubmit}>
+    <div className="wrapper">
+      <form className="formContainer" onSubmit={handleSubmit}>
         <input
           type="text"
           name=""
           id=""
           placeholder="Enter City Name"
-          className={styles.inputLocation}
+          className="inputLocation"
           value={location}
           onChange={handleInputChange}
         />
-        <button className={styles.buttonSearch} type="submit">
+        <button className="buttonSearch" type="submit">
           Search
         </button>
       </form>
@@ -50,20 +49,20 @@ function App() {
       {loading && <p>Loading data…</p>}
 
       {weatherDetails != null && weatherDetails.current && (
-        <div className={styles.weatheDetailsContainer}>
-          <div className={styles.weatherDetailCard}>
+        <div className="weatheDetailsContainer">
+          <div className="weatherDetailCard">
             <h3>Temperature</h3>
             <p>{weatherDetails.current.feelslike_c} °C</p>
           </div>
-          <div className={styles.weatherDetailCard}>
+          <div className="weatherDetailCard">
             <h3>Humidity</h3>
             <p>{weatherDetails.current.humidity}%</p>
           </div>
-          <div className={styles.weatherDetailCard}>
+          <div className="weatherDetailCard">
             <h3>Condition</h3>
             <p>{weatherDetails.current.condition.text}</p>
           </div>
-          <div className={styles.weatherDetailCard}>
+          <div className="weatherDetailCard">
             <h3>Wind Speed</h3>
             <p>{weatherDetails.current.wind_kph} kph</p>
           </div>
